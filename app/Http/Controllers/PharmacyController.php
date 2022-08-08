2b3ce28d36f45e8fb31fb4d113bd08ad2a72ffd8
personal <hesham.mohamed19930@gmail.com>
2022-08-08 17:28:08 +0200

    your comments

--- app/Http/Controllers/PharmacyController.php
+++ app/Http/Controllers/PharmacyController.php
@@ -20,6 +20,8 @@ class PharmacyController extends Controller
         return view('admin.pharmacy.index', compact('pharmacies'));
     }
 
+    // new line
+
     /**
      * Show the form for creating a new resource.
      *
